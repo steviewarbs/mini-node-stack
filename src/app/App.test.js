fe02c75ef3a45f08e5abf70516a5b85f1d1e8562
Your Name <steve.warburton@matillion.com>
2023-02-21 09:51:27 +0000

    updated CCS file and updates tests

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Mini Node Stack text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appHeader = screen.getByText(/Mini Node Stack/i);
+  expect(appHeader).toBeInTheDocument();
 });
